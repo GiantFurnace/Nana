2c32538d8f1963c8c673223f7f3197bee1dc7682
ChenZhengQiang <42955747+SnowInReds@users.noreply.github.com>
2018-09-29 21:36:55 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,10 +10,11 @@ using logging::Nana;
 
 static const int BREAK_TIME = 5;
 static const int LIFE_LENGTH = 1024*1024*10;
+
 int main( int argc, char ** argv )
 {
     Nana * nana = Nana::born( "nana.log", Nana::HAPPY, BREAK_TIME, LIFE_LENGTH );
-    nana->say( Nana::HAPPY, __func__, "A girl named nana born in this world with %d year(s) old", 1);
+    nana->say( Nana::HAPPY, __func__, "A girl named nana born in this world with %s emotion", "HAPPY");
     nana->die();
     return 0;
 }
